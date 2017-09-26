ba05c161783e6c268119904aac26a2db715ea3f3
Jeroen Op 't Eynde <jeroen@unleashed.be>
2017-09-26 13:30:22 +0200

    Bring setup.py version in sync with git tag

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ for req in requirements_test_file:
 
 setup(
     name='wingmonkey',
-    version='0.1.0',
+    version='0.1.4',
     url='https://github.com/vikingco/wingmonkey',
     license='BSD',
     author='Jonas Steur',
