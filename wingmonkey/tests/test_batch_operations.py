abd55de42dffde1975266d3360f051f960b38123
Jonatas CD <jonatas.cd@gmail.com>
2017-11-20 11:13:52 +0100

    Add tests to batch_operations

--- wingmonkey/tests/test_batch_operations.py
+++ wingmonkey/tests/test_batch_operations.py
@@ -5,7 +5,10 @@ from json import dumps
 
 from wingmonkey.settings import MAILCHIMP_ROOT
 from wingmonkey.enums import MemberStatus
-from wingmonkey.batch_operations import batch_add_members, batch_update_members, batch_delete_members
+from wingmonkey.batch_operations import batch_add_members, batch_update_members, batch_delete_members, \
+    BatchOperationResourceSerializer, BatchOperationResource, BatchOperationResourceCollectionSerializer, \
+    BatchOperationResourceCollection
+from wingmonkey.mailchimp_session import MailChimpSession
 from wingmonkey.members import Member
 
 
@@ -17,7 +20,7 @@ def expected_batch_operation_resource():
         'total_operations': 1,
         'finished_operations': 0,
         'errored_operations': 0,
-        'submitted_at': str(datetime.today()),
+        'submitted_at': datetime.strftime(datetime.today(), '%Y-%m-%d %H:%M:%S'),
         'completed_at': None,
         'response_body_url': 'https://amailchimp/link/goes/here',
         '_links': None,
@@ -98,3 +101,69 @@ def test_batch_delete_members(expected_batch_operation_resource, expected_member
         request_mock.post(f'{MAILCHIMP_ROOT}/batches',
                           text=dumps(expected_batch_operation_resource), additional_matcher=match_request_text)
         assert batch_delete_members('alistid1234', members)
+
+
+def test_batch_operation_resource_serializer():
+    serializer = BatchOperationResourceSerializer()
+    assert isinstance(serializer.session, MailChimpSession)
+
+
+def _compare_batch_operation_resource_result(batch, expected=None):
+    if expected is None:
+        return False
+    elif not isinstance(expected, dict):
+        expected = expected.__dict__
+
+    assert batch.id == expected['id']
+    assert batch.status == expected['status']
+    assert batch.total_operations == expected['total_operations']
+    assert batch.finished_operations == expected['finished_operations']
+    assert batch.errored_operations == expected['errored_operations']
+    assert batch.submitted_at == datetime.strptime(expected['submitted_at'], '%Y-%m-%d %H:%M:%S')
+    assert batch.completed_at == expected['completed_at']
+    assert batch.response_body_url == expected['response_body_url']
+    assert batch._links == expected['_links']
+
+    return True
+
+
+def test_batch_operation_resource_serializer_read(expected_batch_operation_resource):
+    serializer = BatchOperationResourceSerializer()
+    batch = BatchOperationResource(**expected_batch_operation_resource)
+    with Mocker() as request_mock:
+        request_mock.get(f'{MAILCHIMP_ROOT}/batches/{expected_batch_operation_resource["id"]}',
+                         text=dumps(expected_batch_operation_resource))
+        assert _compare_batch_operation_resource_result(serializer.read(batch_id=batch.id),
+                                                        expected_batch_operation_resource)
+
+
+def test_batch_operation_resource_serializer_delete(expected_batch_operation_resource):
+    serializer = BatchOperationResourceSerializer()
+    batch = BatchOperationResource(**expected_batch_operation_resource)
+    with Mocker() as request_mock:
+        request_mock.delete(f'{MAILCHIMP_ROOT}/batches/{expected_batch_operation_resource["id"]}', text='')
+        assert serializer.delete(batch.id)
+
+
+@fixture
+def expected_batch_operation_resource_collection(expected_batch_operation_resource):
+    return {
+        'batches': [
+            expected_batch_operation_resource
+        ],
+        'total_items': 1,
+        '_links': None
+    }
+
+
+def test_batch_operation_resource_collection_serializer():
+    serializer = BatchOperationResourceCollectionSerializer()
+    assert isinstance(serializer.session, MailChimpSession)
+
+
+def test_batch_operation_resource_collection(expected_batch_operation_resource_collection):
+    expected = expected_batch_operation_resource_collection
+    collection = BatchOperationResourceCollection(**expected_batch_operation_resource_collection)
+    assert collection.total_items == expected['total_items']
+    assert collection._links == expected['_links']
+    assert collection.batches[0]['id'] == expected['batches'][0]['id']
