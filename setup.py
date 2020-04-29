a1d1675dd0670c6bdf5edeb9f1c715b025c26f61
Niels Van Och <niels.vanoch@gmail.com>
2020-04-29 16:59:17 +0200

    Make install work with latest pip version

--- setup.py
+++ setup.py
@@ -11,21 +11,21 @@ dependency_links = []
 # Inject requirements from requirements.txt into setup.py
 requirements_file = parse_requirements(path.join('requirements', 'requirements.txt'), session=PipSession())
 for req in requirements_file:
-    install_requires.append(str(req.req))
+    install_requires.append(str(req.requirement))
     if req.link:
         dependency_links.append(str(req.link))
 
 # Inject test requirements from requirements_test.txt into setup.py
 requirements_test_file = parse_requirements(path.join('requirements', 'requirements_test.txt'), session=PipSession())
 for req in requirements_test_file:
-    tests_require.append(str(req.req))
+    tests_require.append(str(req.requirement))
     if req.link:
         dependency_links.append(str(req.link))
 
 
 setup(
     name='wingmonkey',
-    version='0.2.1',
+    version='0.2.3',
     url='https://github.com/vikingco/wingmonkey',
     license='BSD',
     author='Jonas Steur',
